be8e6febc813036bd5b51aeb3f677eb50e9d9cc2
Akash <akabhowmick@gmail.com>
2025-02-04 20:44:38 -0500

    change the root email

--- src/utils/ApiKeys.ts
+++ src/utils/ApiKeys.ts
@@ -3,9 +3,7 @@ export const paypalClientId =
   "AXqOomrS73dZYPvV-fElIHCQITsnzIRe0DNmMZKhmIC6nod2TPfhJV9HGnD4iE4O0M4UL3_jAJzgL8gs";
 
 // Email form
-
-const companyEmail = "print3Dverse@gmail.com";
-
+const companyEmail = "queensfinestprints@gmail.com";
 export const contactFormId = `https://formsubmit.co/${companyEmail}`;
 export const imageUploadFormId = `https://formsubmit.co/${companyEmail}`;
 export const orderReviewFormId = `https://formsubmit.co/${companyEmail}`;
